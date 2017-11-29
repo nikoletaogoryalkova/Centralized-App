818383ffbae4edc0d212f007e4642c099701f24a
ChrisPam <chrispam1509@gmail.com>
2017-11-29 22:16:21 +0200

    Fixed bugs with request after refactoring

--- marketplace/js/config1.js
+++ marketplace/js/config1.js
@@ -1,7 +1,7 @@
 function config() {
     var apiHost = "//localhost:8080/";
 
-    var frontEndHost = "//localhost:63342/lockchain-FrontEnd/";
+    var frontEndHost = "//localhost:63342/Lockchain-FrontEnd/";
 
     return [apiHost, frontEndHost];
 }
