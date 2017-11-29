29f99fb8d12f1ac338cab2e055e9bfa50ec0d0a5
iludart <peter.penev@hotmail.com>
2017-11-29 21:03:03 +0200

    added clear filters button

--- marketplace/js/config1.js
+++ marketplace/js/config1.js
@@ -1,7 +1,7 @@
 function config() {
     var apiHost = "//localhost:8080/";
 
-    var frontEndHost = "//localhost:63342/marketplace/Web-Design/marketplace/";
+    var frontEndHost = "//localhost:63342/Web-Design/marketplace/";
 
     return [apiHost, frontEndHost];
 }
