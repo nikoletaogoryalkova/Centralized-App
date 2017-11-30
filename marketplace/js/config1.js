c27ca0d4079ccdd8224a66b5e8dde607dd24879b
ChrisPam <chrispam1509@gmail.com>
2017-11-30 15:56:01 +0200

    Fixed daterangepicker urls, save filters after refresh, fixed footer currency select, fixed query for backend with currency, fixed triggers in datepickers, save guests count in property view and implemented stars visualization and average rating with prefix on listings view

--- marketplace/js/config1.js
+++ marketplace/js/config1.js
@@ -1,7 +1,7 @@
 function config() {
     var apiHost = "//localhost:8080/";
 
-    var frontEndHost = "//localhost:63342/Lockchain-FrontEnd/";
+    var frontEndHost = "//localhost:63342/marketplace/";
 
     return [apiHost, frontEndHost];
 }
