e5a2ad140df4d5f6d8887760784d1755eb78e4cb
ChrisPam <chrispam1509@gmail.com>
2017-12-12 21:08:28 +0200

    Switch users

--- src/components/createListing/basics/CreateListingFacilities.jsx
+++ src/components/createListing/basics/CreateListingFacilities.jsx
@@ -9,8 +9,6 @@ export default class CreateListingFacilities extends React.Component {
             <div>
                 <CreateListingBasicsAside />
                 <h1>CreateListingFacilities</h1>
-
-                
                 <br/>
                 <NavLink to="/listings/create/accommodation" className="btn btn-default" id="btn-continue">Back</NavLink>
                 <NavLink to="/listings/create/safetyamenities" className="btn btn-primary" id="btn-continue">Continue</NavLink>
