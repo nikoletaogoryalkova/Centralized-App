40b364fc2acd59a2b53c91a4a6ac12a8c51804b6
ChrisPam <chrispam1509@gmail.com>
2017-12-14 23:50:54 +0200

    Implemented upload images functionality

--- src/components/createListing/placeDescription/CreateListingPhotos.jsx
+++ src/components/createListing/placeDescription/CreateListingPhotos.jsx
@@ -3,17 +3,99 @@ import { NavLink } from 'react-router-dom';
 
 import CreateListingPlaceDescriptionAside from './CreateListingPlaceDescriptionAside';
 
-import ImagePreview from 'react-image-preview';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+import update from 'react-addons-update';
+
+import { Config } from '../../../config';
+
+const CLOUDINARY_UPLOAD_PRESET = 'kevdtpwc';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dvhembyhr/upload';
+
 
 export default class CreateListingPhotos extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            uploadedFiles: [],
+            uploadedFilesUrls: []
+        };
+
+        this.onImageDrop = this.onImageDrop.bind(this);
+        this.handleImageUpload = this.handleImageUpload.bind(this);
+        this.removePhoto = this.removePhoto.bind(this);
+    }
+
+    onImageDrop(files) {
+        this.handleImageUpload(files)
+
+        this.setState({
+            uploadedFiles: files
+        });
+    }
+
+    handleImageUpload(files) {
+        files.forEach((file) => {
+            let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                .field('file', file);
+
+
+            upload.end((err, response) => {
+                if (err) {
+                    console.error(err);
+                }
+                if (response.body.secure_url !== '') {
+                    this.setState(previousState => ({
+                        uploadedFilesUrls: [...previousState.uploadedFilesUrls, response.body.secure_url]
+                    }));
+                }
+            });
+        });
+    }
+
+    removePhoto(e) {
+        e.preventDefault();
+
+        let imageUrl = e.target.nextSibling.getAttribute('src');
+
+        let indexOfImage = this.state.uploadedFilesUrls.indexOf(imageUrl);
+
+        this.setState({
+            uploadedFilesUrls: update(this.state.uploadedFilesUrls, { $splice: [[indexOfImage, 1]] })
+        });
+    }
+
     render() {
         return (
             <div>
                 <CreateListingPlaceDescriptionAside />
-                <h1>Photos</h1>
+                <div className="reservation-hotel-review-room col-md-9">
+                    <h2>Upload photos of your place</h2>
+                    <hr />
 
-                <ImagePreview />
-                <br/>
+                    <Dropzone
+                        className="pictures-upload"
+                        multiple={true}
+                        accept="image/*"
+                        onDrop={this.onImageDrop}>
+                        <p>Drop files to upload</p>
+                        <button className="btn btn-primary">Choose file</button>
+                    </Dropzone>
+
+                    <div className="pictures-preview col-md-12">
+                        {this.state.uploadedFilesUrls.length === 0 ? null :
+                            this.state.uploadedFilesUrls.map((imageUrl, i) =>
+                                <div key={i} className="uploaded-small-picture col-md-4">
+                                    <button onClick={this.removePhoto} className="close"><img src={Config.getValue("basePath") + "images/icon-delete.png"} /></button>
+                                    <img src={imageUrl} height={200} />
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+                <br />
                 <NavLink to="/listings/create/description" className="btn btn-default" id="btn-continue">Back</NavLink>
                 <NavLink to="/listings/create/houserules" className="btn btn-primary" id="btn-continue">Continue</NavLink>
             </div>
