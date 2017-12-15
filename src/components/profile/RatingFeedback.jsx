34be249e2a5705abc6bd8d8a9d506b22917223b2
Stelios <arianoutsos@gmail.com>
2017-12-15 14:30:45 +0200

    Profile's My Listings Active static template
    Profile's My Listings Active static template

---
+++ src/components/profile/RatingFeedback.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class RatingFeedback extends React.Component {
+    render() {
+        return (
+            <span class="rating-feedback">
+                Excellent 4.1/5
+                <span className="stars-outer stars-cnt stars-bg">
+                    <span className="star"></span>
+                    <span className="star"></span>
+                    <span className="star"></span>
+                    <span className="star"></span>
+                    <span className="star"></span>
+                    <span className="stars-cnt stars-fg" style={{width:'60%'}}> {/* 20% per rated star */}
+                        <span className="stars-cnt stars-over">
+                            <span className="star"></span>
+                            <span className="star"></span>
+                            <span className="star"></span>
+                            <span className="star"></span>
+                            <span className="star"></span>
+                        </span>
+                    </span>
+                </span>
+                78 Reviews
+            </span>
+        );
+    }
+}
