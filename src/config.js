a5268e019354cc6cd8f966576ac537c6755f6c8f
RoYaLBG <royal4o@gmail.com>
2017-12-21 12:45:41 +0200

    leave the react scripts watcher for local development

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
-// import cfgDev from "./config.development";
-// import cfgProd from "./config.production";
-// // let cfg = process.env.NODE_ENV === 'development' ? cfgDev.cfgDev : cfgProd.cfgProd;
 export class Config {
     static getValue(name) {
         return process.env["REACT_APP_" + name];
