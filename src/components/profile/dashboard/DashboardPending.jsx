80807087bdcc992994e0380a75a411db8c32fa58
RoYaLBG <royal4o@gmail.com>
2017-12-29 13:21:04 +0200

    dashboard uses dynamic info

--- src/components/profile/dashboard/DashboardPending.jsx
+++ src/components/profile/dashboard/DashboardPending.jsx
@@ -1,45 +1,55 @@
 import React from 'react';
+import moment from "moment";
 
 export default class DashboardPending extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <section id="profile-dashboard-pending">
                 <div className="container">
-                    <h2>Pending Requests &amp; Discussions (3)</h2>
-                    <hr className="profile-line" />
+                    <h2>Pending Requests ({this.props.totalReservations})</h2>
+                    <hr className="profile-line"/>
                     <ul className="profile-pending-list profile-pending-header bold">
                         <li><span>&nbsp;</span></li>
                         <li><span>Booker</span></li>
                         <li><span>Trip Dates</span></li>
                         <li><span>Nights</span></li>
                         <li><span>Status</span></li>
-                        <li><span>Date</span></li>
+                        <li><span>Price</span></li>
                     </ul>
-                    <ul className="profile-pending-list profile-pending-item">
-                        <li></li>
-                        <li>
+                    {this.props.reservations.map(reservation => {
+                        return (
+                            <ul className="profile-pending-list profile-pending-item">
+                                <li></li>
+                                <li>
                             <span className="cnt block">
-                                <span className="block bold">Jaine</span>
-                                <span className="where block">at Heaven - Suite with view</span>
+                                <span className="block bold">{reservation.guestName}</span>
+                                <span className="where block">{reservation.listingName}</span>
                             </span>
-                        </li>
-                        <li>
-                            <span className="cnt block"><span className="text-green">21</span> oct, 2017 &rarr; <span className="text-d87a61">27</span> oct, 2017</span>
-                        </li>
-                        <li>
-                            <span className="cnt block">99 nights &bull; 99 guests</span>
-                        </li>
-                        <li>
+                                </li>
+                                <li>
+                                        <span className="cnt block">{moment(new Date(reservation.startDate)).format("DD MMM, YYYY")}<i aria-hidden="true" className="fa fa-long-arrow-right"></i>{moment(new Date(reservation.endDate)).format("DD MMM, YYYY")}</span>
+                                </li>
+                                <li>
+                                    <span className="cnt block">{parseInt((new Date(reservation.endDate) - new Date(reservation.startDate)) / (1000 * 60 * 60 * 24))} nights &bull; {reservation.guests} guests</span>
+                                </li>
+                                <li>
                             <span className="cnt block">
                                 <span className="bold">Reservation Request</span>
-                                <span> - </span>
-                                <span>$1.050</span>
+                            <span> - </span>
+                            <span>{reservation.currencyCode} {reservation.price}</span>
                             </span>
-                        </li>
-                        <li>
-                            <span className="cnt block">17 oct</span>
-                        </li>
-                    </ul>
+                                </li>
+                                {/*<li>*/}
+                                    {/*<span className="cnt block">17 oct</span>*/}
+                                {/*</li>*/}
+                            </ul>
+                        )
+                    })}
+
                 </div>
             </section>
         )
