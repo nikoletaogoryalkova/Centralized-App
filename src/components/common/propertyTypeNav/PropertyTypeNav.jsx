c98a5cdd35cbcbcaa2796d295257b7ba80198525
iludart <peter.penev@hotmail.com>
2018-02-12 18:55:15 +0200

    LOC-197 Router structure for homes/hotels is now set up

--- src/components/common/propertyTypeNav/PropertyTypeNav.jsx
+++ src/components/common/propertyTypeNav/PropertyTypeNav.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 
-const PropertyTypeNav = () => (
-    <div>
-        <nav id="second-nav">
-            <div className="container">
-                <ul className="nav navbar-nav">
-                    <li><NavLink to="/hotels" activeClassName="active">HOTELS</NavLink></li>
-                    <li><NavLink to="/homes" activeClassName="active">HOMES</NavLink></li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-);
+function PropertyTypeNav(props) {
+    return (
+        <div>
+            <nav id="second-nav">
+                <div className="container">
+                    <ul className="nav navbar-nav">
+                        <li><NavLink to='/hotels' activeClassName="active">HOTELS</NavLink></li>
+                        <li><NavLink to='/homes' activeClassName="active">HOMES</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
 
-export default PropertyTypeNav;
+export default withRouter(PropertyTypeNav);
