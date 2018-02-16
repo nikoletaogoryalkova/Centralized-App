4491d43fada32d316507d58cb9a03086152fd28d
chris <thcrnk@gmail.com>
2018-02-16 16:30:52 +0200

    Recover user wallet

--- src/services/blockchain/wallet.js
+++ src/services/blockchain/wallet.js
@@ -4,16 +4,15 @@ import bip39 from 'bip39';
 import hdkey from 'ethereumjs-wallet/hdkey';
 
 import { NotificationManager } from 'react-notifications';
+import { validateEtherAddress, validatePassword } from './validators/base-validators';
+
 const { HD_WALLET_PATH } = require('./config/constants.json');
-const { INVALID_PASSWORD } = require('./utils/errors.json');
+const errors = require('./utils/errors.json');
 
 
 class Wallet {
     static createFromPassword(password) {
-        if (password === '') {
-            NotificationManager.error(INVALID_PASSWORD);
-            throw new Error(INVALID_PASSWORD);
-        }
+        validatePassword(password);
 
         const mnemonic = bip39.generateMnemonic();
         const hdWallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
@@ -27,6 +26,45 @@ class Wallet {
         result.jsonFile = localWallet.toV3(password);
 
         return result;
+    }
+
+    static recoverFromMnemonic(mnemonic, password, address) {
+        validatePassword(password);
+        this.validateAddress(address);
+        this.validateMnemonic(mnemonic);
+
+        const hdWallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+        const wallet = hdWallet.derivePath(HD_WALLET_PATH).getWallet();
+
+        let result = {};
+        result.address = wallet.getAddressString();
+
+        if (result.address !== address) {
+            throw errors.INVALID_RECOVERED_ADDRESS;
+        }
+
+        result.mnemonic = mnemonic;
+        result.fileName = wallet.getV3Filename();
+        result.jsonFile = wallet.toV3(password);
+
+        return result;
+    }
+
+    static validateAddress(address) {
+        if (!validateEtherAddress(address)) {
+            NotificationManager.error(errors.INVALID_RECOVERED_ADDRESS);
+            throw new Error(errors.INVALID_RECOVERED_ADDRESS);
+        }
+
+        return true;
+    }
+
+    static validateMnemonic(mnemonic) {
+        if (!bip39.validateMnemonic(mnemonic)) {
+            throw errors.INVALID_MNEMONIC;
+        }
+
+        return true;
     }
 }
 
