37b683a0e4a01e156f102d48c71aa89bd3c098e2
iludart <peter.penev@hotmail.com>
2018-04-25 10:19:46 +0300

    Refactored footer and home page components

--- src/components/footer/AttachedFooter.jsx
+++ src/components/footer/AttachedFooter.jsx
@@ -4,87 +4,87 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { setCurrency, setLocRate } from '../../actions/paymentInfo';
-import {getLocRateInUserSelectedCurrency} from '../../requester';
+import { getLocRateInUserSelectedCurrency } from '../../requester';
 
 class AttachedFooter extends React.Component {
-    componentDidMount() {
-        const { currency, locRate } = this.props.paymentInfo;
-        if (localStorage['currency']) setCurrency(localStorage['currency']);
-        else localStorage['currency'] = currency;
+  componentDidMount() {
+    const { currency, locRate } = this.props.paymentInfo;
+    if (localStorage['currency']) setCurrency(localStorage['currency']);
+    else localStorage['currency'] = currency;
 
-        if (!locRate) this.getAndSetLocRate(currency);
+    if (!locRate) this.getAndSetLocRate(currency);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { currency, locRate } = nextProps.paymentInfo;
+    if (!locRate || currency !== this.props.paymentInfo.currency) {
+      this.getAndSetLocRate(currency);
+      localStorage['currency'] = currency;
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        const { currency, locRate } = nextProps.paymentInfo;
-        if (!locRate || currency !== this.props.paymentInfo.currency) {
-            this.getAndSetLocRate(currency);
-            localStorage['currency'] = currency;
-        }
-    }
+  getAndSetLocRate(currency) {
+    getLocRateInUserSelectedCurrency(currency).then((data) => {
+      this.props.dispatch(setLocRate(data[0][`price_${currency.toLowerCase()}`]));
+    });
+  }
 
-    getAndSetLocRate(currency) {
-        getLocRateInUserSelectedCurrency(currency).then((data) => {
-            this.props.dispatch(setLocRate(data[0][`price_${currency.toLowerCase()}`]));
-        });
-    }
+  render() {
+    return (
+      <footer id="attached-footer" className="navbar navbar-fixed-bottom">
+        <div className="container">
+          <div className="row">
 
-    render() {
-        return (
-            <footer id="attached-footer" className="navbar navbar-fixed-bottom">
-                <div className="container">
-                    <div className="row">
+            <div className="col-md-3"></div>
 
-                        <div className="col-md-3"></div>
+            <div className="col-md-3"></div>
 
-                        <div className="col-md-3"></div>
+            <div className="col-md-2"></div>
 
-                        <div className="col-md-2"></div>
+            <div className="col-md-2">
+              <div className="dropdown select-language">
+                <a className="dropdown-toggle" data-toggle="dropdown">English newest</a>
+                <ul className="navbar-dropdown dropdown-menu">
+                  <li><a >English</a></li>
+                  <li><a >English</a></li>
+                </ul>
+              </div>
+            </div>
 
-                        <div className="col-md-2">
-                            <div className="dropdown select-language">
-                                <a className="dropdown-toggle" data-toggle="dropdown">English newest</a>
-                                <ul className="navbar-dropdown dropdown-menu">
-                                    <li><a >English</a></li>
-                                    <li><a >English</a></li>
-                                </ul>
-                            </div>
-                        </div>
+            <div className="col-md-2">
+              <select
+                onChange={(e) => this.props.dispatch(setCurrency(e.target.value))}
+                value={this.props.paymentInfo.currency}
+                className="currency-switcher">
+                <option value="GBP">£ GBP</option>
+                <option value="EUR">€ EUR</option>
+                <option value="USD">$ USD</option>
+              </select>
 
-                        <div className="col-md-2">
-                            <select
-                                onChange={(e) => this.props.dispatch(setCurrency(e.target.value))}
-                                value={this.props.paymentInfo.currency}
-                                className="currency-switcher">
-                                <option value="GBP">£ GBP</option>
-                                <option value="EUR">€ EUR</option>
-                                <option value="USD">$ USD</option>
-                            </select>
-
-                            {/* <DropdownButton block={true} title={localStorage['currency'] ? localStorage['currency'] : 'USD'} id="bg-nested-dropdown">
+              {/* <DropdownButton block={true} title={localStorage['currency'] ? localStorage['currency'] : 'USD'} id="bg-nested-dropdown">
                                 <MenuItem onClick={() => this.toggleCurrency('GBP')}>£ GBP</MenuItem>
                                 <MenuItem onClick={() => this.toggleCurrency('EUR')}>€ EUR</MenuItem>
                                 <MenuItem onClick={() => this.toggleCurrency('USD')}>$ USD</MenuItem>
                             </DropdownButton> */}
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        );
-    }
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 
 AttachedFooter.propTypes = {
-    // start Redux props
-    dispatch: PropTypes.func,
-    paymentInfo: PropTypes.object
+  // start Redux props
+  dispatch: PropTypes.func,
+  paymentInfo: PropTypes.object
 };
 
 export default connect(mapStateToProps)(AttachedFooter);
 
 function mapStateToProps(state) {
-    const { paymentInfo } = state;
-    return {
-        paymentInfo
-    };
+  const { paymentInfo } = state;
+  return {
+    paymentInfo
+  };
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <footer id="main-footer" className="clearfix">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-3">
-                            <h3>LockTrip</h3>
-                            <ul>
-                                <li><a >Help</a></li>
-                                <li><a >Terms and Conditions</a></li>
-                                <li><a >Legal Information</a></li>
-                                <li><a >Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-md-3">
-                            <h3>Hosting</h3>
-                            <ul>
-                                <li><a >Why Host</a></li>
-                                <li><a >Hospitality</a></li>
-                                <li><a >Responsible Hosting</a></li>
-                                <li><a >Community Center</a></li>
-                            </ul>
-                        </div>
+  render() {
+    return (
+      <footer id="main-footer" className="clearfix">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <h3>LockTrip</h3>
+              <ul>
+                <li><a >Help</a></li>
+                <li><a >Terms and Conditions</a></li>
+                <li><a >Legal Information</a></li>
+                <li><a >Privacy Policy</a></li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <h3>Hosting</h3>
+              <ul>
+                <li><a >Why Host</a></li>
+                <li><a >Hospitality</a></li>
+                <li><a >Responsible Hosting</a></li>
+                <li><a >Community Center</a></li>
+              </ul>
+            </div>
 
-                        <div className="col-md-2"></div>
+            <div className="col-md-2"></div>
 
-                        <div className="col-md-2"></div>
+            <div className="col-md-2"></div>
 
-                        <div className="col-md-2"></div>
+            <div className="col-md-2"></div>
 
-                        <div className="clearfix"></div>
-                    </div>
-                    <div className="copyright col-md-6">Copyright 2017-2018 LockTrip | All rights reserved.</div>
+            <div className="clearfix"></div>
+          </div>
+          <div className="copyright col-md-6">Copyright 2017-2018 LockTrip | All rights reserved.</div>
 
-                </div>
-            </footer>
-        );
-    }
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
