a1ba580c26ece6becf4809740d020a1deaa9f1a8
iludart <peter.penev@hotmail.com>
2018-05-14 10:11:13 +0300

    Added additional error handling in main nav

--- src/components/profile/admin/AllListingsFilter.jsx
+++ src/components/profile/admin/AllListingsFilter.jsx
@@ -68,7 +68,7 @@ export default class AllListingsFilter extends React.Component {
             </div>
           </div>
           <div className="col-md-2">
-            <div className="form-group">
+            <div>
               <button type="submit" className="btn btn-block btn-primary">Search</button>
             </div>
           </div>
